6eef71b063fc28d80e4feafea7b9f82671c29095
thvpoly <161834442+thvpoly@users.noreply.github.com>
2025-10-08 07:46:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 const questions = [
     {
-      question: "Qual é o maior animal do mundo?",
+      question: "Qual é o principal objetivo da fermentação no processo de produção do café?",
       answers: [
-        { id: 1, text: "Tubarão", correct: false },
-        { id: 2, text: "Baleia Azul", correct: true },
-        { id: 3, text: "Elefante", correct: false },
-        { id: 4, text: "Girafa", correct: false },
+        { id: 1, text: "Melhorar o sabor do café com acidez excessivao", correct: false },
+        { id: 2, text: "Desenvolver aromas e sabores mais complexos no café", correct: true },
+        { id: 3, text: "Acelerar a torra dos grãos", correct: false },
+        { id: 4, text: "Reduzir o custo de produção", correct: false },
       ],
     },
     {
-      question: "Qual é o maior deserto do mundo?",
+      question: "Qual é o método de preparação de café que utiliza pressão para forçar a água quente através do pó de café finamente moído?",
       answers: [
-        { id: 1, text: "Kalahari", correct: false },
-        { id: 2, text: "Gobi", correct: false },
-        { id: 3, text: "Saara", correct: true },
-        { id: 4, text: "Antartica", correct: false },
+        { id: 1, text: "French Press", correct: false },
+        { id: 2, text: "Aeropress", correct: false },
+        { id: 3, text: "Espresso", correct: true },
+        { id: 4, text: "V60", correct: false },
       ],
     },
     {
-      question: "Qual é o menor continente do mundo?",
+      question: "O que caracteriza um café de torra clara",
       answers: [
-        { id: 1, text: "Asia", correct: false },
-        { id: 2, text: "Austrália", correct: true },
-        { id: 3, text: "Ártico", correct: false },
-        { id: 4, text: "Áfria", correct: false },
+        { id: 1, text: "O café tem sabores mais amargos e fortes", correct: false },
+        { id: 2, text: "O café mantém mais notas de acidez e sabores originários do grão.", correct: true },
+        { id: 3, text: "O café perde completamente as notas originais e ganha sabor mais doce", correct: false },
+        { id: 4, text: "A torra clara é utilizada apenas para cafés com alto teor de cafeína", correct: false },
       ],
     },
     {
-      question: "Qual é o menor país do mundo mundo?",
+      question: "Qual dos seguintes tipos de grão de café é mais comum no Brasil?",
       answers: [
-        { id: 1, text: "Vaticano", correct: true },
-        { id: 2, text: "Butão", correct: false },
-        { id: 3, text: "Nepal", correct: false },
-        { id: 4, text: "Shri Lanka", correct: false },
+        { id: 1, text: "Arábica", correct: true },
+        { id: 2, text: "Robusta", correct: false },
+        { id: 3, text: "Maragogipe", correct: false },
+        { id: 4, text: "Liberica", correct: false },
       ],
     },
   ];
